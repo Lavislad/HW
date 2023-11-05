c98cee75bbe50e2001a061053d9629e6060c7434
Lavislad <143618582+Lavislad@users.noreply.github.com>
2023-11-05 16:06:55 +0300

    Урок "ORM и Django Shell"

---
+++ advertisments/app_advertisments/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import index
+from .views import top_sellers
+from .views import register
+from .views import advertisement_post
+from .views import advertisement
+
+#Ссылки на страницы
+urlpatterns = [
+    path('', index),
+    path('top-sellers.html', top_sellers),
+    path('register.html', register),
+    path('advertisement-post.html', advertisement_post),
+    path('advertisement.html', advertisement),
+]
